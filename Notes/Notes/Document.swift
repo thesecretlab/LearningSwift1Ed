0f4e1342319ca55d8a566ab4aa63b42a9d17b1bf
Jonathon Manning <jon@secretlab.com.au>
2015-08-24 17:52:00 +1000

    Re-architected to make documents use bundles

--- Notes/Notes/Document.swift
+++ Notes/Notes/Document.swift
@@ -8,17 +8,49 @@
 
 import Cocoa
 
-let NotesErrorDomain = "NotesErrorDomain"
+// We can be throwing a lot of errors in this class, and they'll all be in the same error domain and using error codes from the same enum, so here's a little convenience func to save typing and space
+private func err(code: ErrorCode, _ userInfo:[NSObject:AnyObject]?=nil)  -> NSError {
+    return NSError(domain: ErrorDomain, code: code.rawValue, userInfo: userInfo)
+}
 
-enum NotesErrorCode : Int {
-    case CannotStoreRTF
-    case CannotLoadRTF
+private let ErrorDomain = "NotesErrorDomain"
+
+// Used for looking up data in Document.plist
+private enum DocumentSummaryKeys : String {
+    case Attachments = "attachments" // -> [String]
+}
+
+// Names of files/directories in the package
+private enum FileNames : String {
+    case DocumentFile = "Document.plist"
+    case TextFile = "Text.rtf"
+    case AttachmentsDirectory = "Attachments"
+}
+
+// Things that can go wrong.
+private enum ErrorCode : Int {
+    case CannotLoadFileWrappers
+    case CannotLoadText
+    case CannotAccessAttachments
+    case CannotSaveText
 }
 
 class Document: NSDocument {
     
     // Main text content
     var text : NSAttributedString = NSAttributedString()
+    
+    // Attachments
+    var attachedFiles : [String:NSFileWrapper]? {
+        if let attachmentsDirectory = self.documentFileWrapper.fileWrappers?[FileNames.AttachmentsDirectory.rawValue], let attachmentsFileWrappers = attachmentsDirectory.fileWrappers {
+            return attachmentsFileWrappers
+        } else {
+            return nil
+        }
+    }
+    
+    // Directory file wrapper
+    var documentFileWrapper = NSFileWrapper(directoryWithFileWrappers: [:])
 
     override init() {
         super.init()
@@ -39,34 +71,48 @@ class Document: NSDocument {
         // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
         return "Document"
     }
-
-    override func dataOfType(typeName: String) throws -> NSData {
+    
+    override func readFromFileWrapper(fileWrapper: NSFileWrapper, ofType typeName: String) throws {
         
-        // Attempt to convert the data into an NSData object that contains an RTFD representation of the document's text.
-        if let RTFData = text.RTFDFromRange(NSRange(0..<text.length), documentAttributes: [:]) {
-            return RTFData
-        } else {
-            // If that fails, throw an error.
-            throw NSError(domain: NotesErrorDomain, code: NotesErrorCode.CannotStoreRTF.rawValue, userInfo: nil)
+        
+        
+        // Ensure that we have additional file wrappers in this file wrapper
+        guard let fileWrappers = fileWrapper.fileWrappers else {
+            throw err(.CannotLoadFileWrappers)
         }
+        
+        // Ensure that we can access the document text
+        guard let documentTextData = fileWrappers[FileNames.TextFile.rawValue]?.regularFileContents else {
+            throw err(.CannotLoadText)
+        }
+        
+        // Load the text data as RTF
+        
+        guard let documentText = NSAttributedString(RTF: documentTextData, documentAttributes: nil) else {
+            throw err(.CannotLoadText)
+        }
+        
+        // Keep the text in memory
+        self.text = documentText
+        
+    }
+    
+    override func fileWrapperOfType(typeName: String) throws -> NSFileWrapper {
+        
+        let textRTFData = try self.text.dataFromRange(NSRange(0..<self.text.length), documentAttributes: [NSDocumentTypeDocumentAttribute:NSRTFTextDocumentType])
+        
+        if let oldTextFileWrapper = self.documentFileWrapper.fileWrappers?[FileNames.TextFile.rawValue] {
+            self.documentFileWrapper.removeFileWrapper(oldTextFileWrapper)
+            
+        }
+        
+        self.documentFileWrapper.addRegularFileWithContents(textRTFData, preferredFilename: FileNames.TextFile.rawValue)
+        
+        return self.documentFileWrapper
+        
         
     }
 
-    override func readFromData(data: NSData, ofType typeName: String) throws {
-        
-        // Attempt to convert the provided data into an attributed string.
-        if let attributedString = NSAttributedString(RTFD: data, documentAttributes: nil) {
-            
-            // If it worked, store it
-            self.text = attributedString
-        } else {
-            
-            // Otherwise, throw an error
-            throw NSError(domain: NotesErrorDomain, code: NotesErrorCode.CannotLoadRTF.rawValue, userInfo: nil)
-        }
-        
-        
-    }
 
 
 }
